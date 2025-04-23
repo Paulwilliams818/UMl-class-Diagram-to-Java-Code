7119e8884ea25d620800f4ebb50ae33e960fcb35
Paulwilliams818 <paulwilliams.d@eagles.oc.edu>
2025-04-22 20:43:39 -0500

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,24 @@
+public class Admin {
+
+	private String adminId;
+	private String name;
+
+	/**
+	 * 
+	 * @param customerId
+	 */
+	public ActivityLog viewCustomerActivity(String customerId) {
+		// TODO - implement Admin.viewCustomerActivity
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param customerId
+	 */
+	public void assistLoginIssues(String customerId) {
+		// TODO - implement Admin.assistLoginIssues
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ DeliveryPreference.java
@@ -0,0 +1,16 @@
+public class DeliveryPreference {
+
+	private String preferenceId;
+	private String method;
+	private Boolean feeApplicable;
+
+	/**
+	 * 
+	 * @param method
+	 */
+	public void setPreference(String method) {
+		// TODO - implement DeliveryPreference.setPreference
+		throw new UnsupportedOperationException();
+	}
+
+}
