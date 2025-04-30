e69abacfad810d23c4deaa18cd16b1c272e17361
Paulwilliams818 <paulwilliams.d@eagles.oc.edu>
2025-04-29 21:13:13 -0500

    Create ActivityLog.java

---
+++ src/main/java/ActivityLog.java
@@ -0,0 +1,16 @@
+public class ActivityLog {
+
+	private String logId;
+	private String action;
+	private Date timestamp;
+
+	/**
+	 * 
+	 * @param customerId
+	 */
+	public List<ActivityLog> getLogsByCustomer(String customerId) {
+		// TODO - implement ActivityLog.getLogsByCustomer
+		throw new UnsupportedOperationException();
+	}
+
+}
