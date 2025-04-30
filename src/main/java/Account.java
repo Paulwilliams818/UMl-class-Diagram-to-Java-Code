00d4738366a8f10e9a66936bd738e455a10443ce
Paulwilliams818 <paulwilliams.d@eagles.oc.edu>
2025-04-29 21:12:49 -0500

    Create Account.java

---
+++ src/main/java/Account.java
@@ -0,0 +1,12 @@
+public class Account {
+
+	private String accountNumber;
+	private String accountType;
+	private String deliveryFrequency;
+
+	public List<Document> getStatements() {
+		// TODO - implement Account.getStatements
+		throw new UnsupportedOperationException();
+	}
+
+}
