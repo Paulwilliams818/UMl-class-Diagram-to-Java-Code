fcc6a9fd3a1fbe119041ad6574e1a15ce2205acf
Paulwilliams818 <paulwilliams.d@eagles.oc.edu>
2025-04-29 20:30:02 -0500

    Delete src directory

--- src/account.js
+++
@@ -1,5 +0,0 @@
-export class Account {
-  fetchDocuments() {
-    return ['doc1.pdf', 'doc2.pdf'];
-  }
-}
--- src/report.js
+++
@@ -1,9 +0,0 @@
-export class Report {
-  constructor(session) {
-    this.session = session;
-  }
-
-  generate() {
-    return this.session?.user === 'admin' ? 'Report generated' : 'Permission denied';
-  }
-}
